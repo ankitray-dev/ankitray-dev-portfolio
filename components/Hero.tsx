20da22ab91a34c026d22caf3edd1d5f04ed1c981
AnkitRay-Dev <164015889+ankitray-dev@users.noreply.github.com>
2024-07-16 12:03:59 +0530

    Initial commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,7 +12,6 @@ const Hero = () => {
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue"/>
       </div>
       <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
-      {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
         </div>
         <div className="flex justify-center relative my-20 z-10">
@@ -27,10 +26,9 @@ const Hero = () => {
                 <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
                     Hi! I&apos;m Ankit, a Fullstack Developer based in India.
                 </p>
-                
-                <a href="#about">
+                <a href="data\Ankit_Ray_Resume.pdf" download="Ankit_Ray_Resume.pdf">
                     <MagicButton
-                    title="Show my work"
+                    title="My resume"
                     icon={<FaLocationArrow />}
                     position="right"
                     />
